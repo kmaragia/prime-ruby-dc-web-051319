3df59da3de9ff958a3a02bc86da63da56f6a149f
Kemunto Maragia <kmaragia@email.wm.edu>
2020-09-11 01:58:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime? (num)
+  if num < 0 
+    return false
+  elsif num == 1 || num == 0 
+    return false
+  elsif num % 2 && num != 2
+    return false
+  elsif num == 2 || num % 2 ==1
+    return true
+  end
+end
